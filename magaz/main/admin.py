0675ae4fbcf039544d33a8cf78dff71f1b67a979
RafMav <rafmav@bk.ru>
2021-10-02 21:53:41 +0400

    minor updates

--- magaz/main/admin.py
+++ magaz/main/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from .models import Category, Product
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
 admin.site.register(Category, CategoryAdmin)
 
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'price']
     prepopulated_fields = {'slug': ('name',)}
 admin.site.register(Product, ProductAdmin)
-
--- magaz/main/models.py
+++ magaz/main/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=250, db_index=True)
-    slug = models.SlugField(max_length=250, db_index=True, unique=True)
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
 
     class Meta:
         ordering = ('name',)
@@ -15,9 +15,9 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=250, db_index=True, verbose_name='Название товара')
-    slug = models.SlugField(max_length=250, db_index=True, unique=True)
+    category = models.ForeignKey(Category, related_name='products', null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=250, verbose_name='Название товара')
+    slug = models.SlugField(max_length=250, unique=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, verbose_name='Изображение')
     description = models.TextField(blank=True, verbose_name='Описание')
     price = models.DecimalField(max_digits=10, decimal_places=2)
