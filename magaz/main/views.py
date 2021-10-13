fe52700a766f7ad1978339527ace47575dc3f883
rafmav <rafmav@bk.ru>
2021-10-13 07:47:20 +0300

    удален лишний код

--- magaz/main/views.py
+++ magaz/main/views.py
@@ -2,13 +2,6 @@ from django.shortcuts import render
 from .models import Product
 
 
-def base(request):
-    return render(
-        request=request,
-        template_name='main/base.html',
-    )
-
-
 def product_list(request):
     product = Product.objects.all()
     return render (request,'main/base.html', {'products': product})
